a79ed1a92c64774b56c3dfc841dfc0665aeede64
shalik06 <mdshalik486@gmail.com>
2025-01-31 21:12:17 +0530

    fill added

---
+++ first.js
@@ -0,0 +1,5 @@
+// JavaScript for form submission
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for contacting me! I will get back to you soon.');
+});
